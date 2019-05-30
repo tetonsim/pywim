154a223a7c4cba05f3aa5e5f1c1a6b9e17e2d305
brady <brady.m.adams@gmail.com>
2019-05-30 09:57:59 -0600

    gitignore caused missed directory - fixed

---
+++ pywim/micro/build/job.py
@@ -0,0 +1,54 @@
+from ... import am, micro, model
+from typing import Union
+
+def Hexpack(composite : micro.Composite):
+    hexpack = micro.Hexpack(composite.volume_fraction)
+    job = micro.Job('hexpack', hexpack)
+
+    job.materials.extend([
+        micro.JobMaterial.FromMaterial('fiber', composite.fiber.name),
+        micro.JobMaterial.FromMaterial('matrix', composite.matrix.name)
+    ])
+
+    return job
+
+def Particulate(composite : micro.Composite):
+    part = micro.ParticulateBCC(composite.volume_fraction)
+    job = micro.Job('particulate', part)
+
+    job.materials.extend([
+        micro.JobMaterial.FromMaterial('fiber', composite.fiber.name),
+        micro.JobMaterial.FromMaterial('matrix', composite.matrix.name)
+    ])
+
+    return job
+
+def ShortFiber(hexpack : micro.Job, particulate : micro.Job, composite : micro.Composite):
+    sf = micro.ShortFiber(composite.volume_fraction, composite.L_over_D)
+    job = micro.Job('short_fiber', sf)
+
+    job.materials.extend([
+        micro.JobMaterial.FromJob('infinite', hexpack.name),
+        micro.JobMaterial.FromJob('end', particulate.name)
+    ])
+
+    return job
+
+def ExtrudedLayer(source : Union[model.Material, micro.Job], config : am.Config):
+    layer = micro.ExtrudedLayer(config)
+    job = micro.Job('layer', layer)
+
+    if isinstance(source, model.Material):
+        job.materials.append( micro.JobMaterial.FromMaterial('plastic', source.name) )
+    else:
+        job.materials.append( micro.JobMaterial.FromJob('plastic', source.name) )
+
+    return job
+
+def Infill(layer : micro.Job, config : am.Config):
+    infill = micro.Infill.FromConfig(config)
+    job = micro.Job('infill', infill)
+
+    job.materials.append( micro.JobMaterial.FromJob('extrusion', layer.name) )
+
+    return job
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 import setuptools
 
-version = '19.0.13'
+version = '19.0.14'
 
 #with open('requirements.txt', 'r') as freq:
 #    requirements = freq.readlines()
