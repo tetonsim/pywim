d5e3815a20ba5ace8f6e074667e51005e2d4cf17
Rick Dalgarno <rick.dalgarno@tetoncomposites.com>
2019-10-03 16:03:24 -0600

    updated per 1st code review

--- pywim/abaqus/wimfea_to_odb.py
+++ pywim/abaqus/wimfea_to_odb.py
@@ -1,120 +1,127 @@
 """ 
-This script reads data from pywim input (.json) and results files (.rst)
-and writes an Abaqus results file (.odb). It converts from from pywim to odb.
+This script converts from pywim to odb.
+It reads data from pywim model (.json) and results files (.rst)
+and writes an Abaqus results file (.odb).
 
-Author: Rick Dalgarno
-Date: 9/30/19
+Usage: abaqus python wimfea_to_odb.py wim_filename
+
+Requirements: 
+1. wim input and output filenames must match
+2. Abaqus must be installed
+
+Supported features:
+1. multiple steps
+2. multiple increments
+3. element types
+    a. 8-node hex, full integration, non-layered
+4. translated field outputs
+    a. stress
+    b. displacement
+
+Future enchancements:
+1. add support for layered 8-node hex
+2. translate strains, failure index, etc. 
 """
 
+import sys
 import json
-
-filename = 'two_sections' # this is the only variable to define
-
-
-### get nodes and elements from JSON input file ###
-
-with open(filename + '.json', 'r') as f:
-    input = json.load(f)
-
-json_nodes = input['mesh']['nodes']
-json_elements = input['mesh']['elements'][0]['connectivity']
-
-# convert a list-of-lists to a tuple-of-tuples
-abq_nodes = tuple(tuple(x) for x in json_nodes)
-abq_elements = tuple(tuple(x) for x in json_elements)
-
-
-### get node labels and displacements from JSON results file ###
-
-with open(filename + '.json.rst', 'r') as h:
-    output = json.load(h)
-
-node_results = output['steps'][0]['increments'][0]['node_results']
-
-for x in node_results:
-    if x['name'] == 'displacement':
-        disp_values = x['values']
-
-node_labels = []
-node_displacements = []
-
-for x in disp_values: # generate list of node labels and list of node displacements
-    node_label = x['id']
-    node_disp = tuple(x['data'])
-    node_labels.append(node_label)
-    node_displacements.append(node_disp)
-
-abq_node_labels = tuple(node_labels)
-abq_displacements = tuple(node_displacements)
-
-
-### get element labels and gauss point stresses from JSON results file ###
-
-gp_results = output['steps'][0]['increments'][0]['gauss_point_results']
-
-for x in gp_results:
-    if x['name'] == 'stress':
-        stress_values = x['values']
-
-element_labels = []
-gp_stresses = []
-
-for x in stress_values: # generate list of element labels
-    element_label = x['id']
-    element_labels.append(element_label)
-
-for j in range(len(stress_values)):
-    wim_values = {}
-    abq_values = {}
-    count = 0
-    for y in stress_values[j]['values']: # reorder gauss point values
-        count += 1
-        wim_values[count] = y['data']
-    abq_values[1] = wim_values[1]
-    abq_values[2] = wim_values[5]
-    abq_values[3] = wim_values[3]
-    abq_values[4] = wim_values[7]
-    abq_values[5] = wim_values[2]
-    abq_values[6] = wim_values[6]
-    abq_values[7] = wim_values[4]
-    abq_values[8] = wim_values[8]
-    for k in range(1, 9, 1): # add ordered gp values to gp list
-        gp_stresses.append(abq_values[k])
-
-
-abq_element_labels = tuple(element_labels)
-abq_gp_stresses = tuple(tuple(x) for x in gp_stresses)
-
-
-### Create an Abaqus odb file ###
-
 from abaqusConstants import *
 from odbAccess import *
 
-odb = Odb(name=filename, path=filename + '_translated.odb')
+def translate_wim_files(wim_model_name):
+    with open(wim_model_name + '.json', 'r') as f:
+        wim_model = json.load(f)
+    with open(wim_model_name + '.json.rst', 'r') as h:
+        wim_result = json.load(h)
 
-# create the Model Data
-part = odb.Part(name='part-1', embeddedSpace=THREE_D, type=DEFORMABLE_BODY)
+    odb = wim_to_odb(wim_model, wim_result, wim_model_name)
+    odb.save()
 
-part.addNodes(nodeData=abq_nodes, nodeSetName='all_nodes')
-part.addElements(elementData=abq_elements, type='C3D8', elementSetName='all_elements')
+def wim_to_odb(wim_model, wim_result, wim_model_name):
+    odb = Odb(name=wim_model_name, path=wim_model_name + '_translated_temp.odb')
 
-a = odb.rootAssembly
-instance = a.Instance(name='part-1-1', object=part)
+    instance = _add_mesh_to_odb(wim_model, odb)
+    
+    _add_results_to_odb(wim_result, odb, instance)
 
-# create the Results Data 
-step = odb.Step(name='step-1', description='', domain=TIME, timePeriod=1.0)
+    return odb
 
-frame = step.Frame(incrementNumber=1, frameValue=1.0)
+def _add_mesh_to_odb(wim_model, odb):
+    json_nodes = wim_model['mesh']['nodes']
+    json_elements = wim_model['mesh']['elements'][0]['connectivity']
+    
+    abq_nodes = tuple(tuple(x) for x in json_nodes)
+    abq_elements = tuple(tuple(x) for x in json_elements)
 
-# write displacements to the odb
-disp_field = frame.FieldOutput(name='U', description='Displacement', type=VECTOR, validInvariants=(MAGNITUDE,))
+    part = odb.Part(name='part-1', embeddedSpace=THREE_D, type=DEFORMABLE_BODY)
 
-disp_field.addData(position=NODAL, instance=instance, labels=abq_node_labels, data=abq_displacements)
+    part.addNodes(nodeData=abq_nodes, nodeSetName='all_nodes')
+    part.addElements(elementData=abq_elements, type='C3D8', elementSetName='all_elements')
 
-# write the stress tensor to the odb
-stress_field = frame.FieldOutput(name='S', description='Stress', type=TENSOR_3D_FULL, validInvariants=(MISES,))
+    instance = odb.rootAssembly.Instance(name='part-1-1', object=part)
 
-stress_field.addData(position=INTEGRATION_POINT, instance=instance, labels=abq_element_labels, data=abq_gp_stresses)
+    return instance
 
-odb.save()
+def _add_results_to_odb(wim_result, odb, instance):
+    for wim_step in wim_result['steps']:
+        _add_step_results_to_odb(wim_step, odb, instance)
+
+def _add_step_results_to_odb(wim_step, odb, instance):
+    odb_step = odb.Step(name=str(wim_step['name']), description='', domain=TIME, timePeriod=1.0)
+
+    inc_num = 0
+    for inc in wim_step['increments']:
+        inc_num += 1
+        _add_inc_results_to_odb_step(inc, odb_step, inc_num, instance)
+
+def _add_inc_results_to_odb_step(inc, odb_step, inc_num, instance):
+    odb_frame = odb_step.Frame(incrementNumber=inc_num, frameValue=1.0)
+
+    # add displacements to odb_frame
+    node_results = inc['node_results']
+
+    for x in node_results:
+        if x['name'] == 'displacement':
+            disp_values = x['values']
+
+    abq_node_labels = tuple(n['id'] for n in disp_values)
+    abq_displacements = tuple(tuple(n['data']) for n in disp_values)
+
+    disp_field = odb_frame.FieldOutput(name='U', description='Displacement', type=VECTOR, validInvariants=(MAGNITUDE,))
+    disp_field.addData(position=NODAL, instance=instance, labels=abq_node_labels, data=abq_displacements)
+
+    # add stresses to odb_frame
+    gp_results = inc['gauss_point_results']
+
+    for x in gp_results:
+        if x['name'] == 'stress':
+            stress_values = x['values']
+
+    gp_stresses = []
+
+    for e in stress_values:
+        wim_values = [gp['data'] for gp in e['values']]
+        abq_values = [
+            wim_values[0],
+            wim_values[4],
+            wim_values[2],
+            wim_values[6],
+            wim_values[1],
+            wim_values[5],
+            wim_values[3],
+            wim_values[7]
+        ]
+        gp_stresses.extend(abq_values)
+
+    abq_element_labels = tuple(n['id'] for n in stress_values)
+    abq_gp_stresses = tuple(tuple(x) for x in gp_stresses)
+
+    stress_field = odb_frame.FieldOutput(name='S', description='Stress', type=TENSOR_3D_FULL, validInvariants=(MISES,))
+    stress_field.addData(position=INTEGRATION_POINT, instance=instance, labels=abq_element_labels, data=abq_gp_stresses)
+
+def main():
+    # add error message if user does not supply -- filename argument
+    translate_wim_files(sys.argv[-1])
+
+if __name__ == '__main__':
+    main()
