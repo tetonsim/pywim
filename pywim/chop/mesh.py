0235bb78f3a621c35156a9dc495c7dab15e6e9eb
dougkenik <doug.kenik@tetonsim.com>
2019-12-19 16:01:09 -0700

    changed none to empty

--- pywim/chop/mesh.py
+++ pywim/chop/mesh.py
@@ -12,10 +12,10 @@ class MeshType(enum.Enum):
     cutting = 3
 
 class MaterialNames(WimObject):
-    def __init__(self, extrusion=None, infill=None, none=None):
+    def __init__(self, extrusion=None, infill=None, empty=None):
         self.extrusion = extrusion if extrusion else 'extrusion'
         self.infill = infill if infill else 'infill'
-        self.none = none if none else 'none'
+        self.empty = empty if empty else 'empty'
 
 class Mesh(WimObject, threemf.mesh.Mesh):
     def __init__(self, name=None):
